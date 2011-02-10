697eee4e4e39897b41e9997d7596d822960b13c4
Daniel P. Berrange <berrange@redhat.com>
2011-02-10 16:06:10 +0000

    Force creation of publican directory
    * autobuild.sh: Force creation of publican directory

--- autobuild.sh
+++ autobuild.sh
@@ -9,6 +9,7 @@ make clean || :
 
 make
 rm -f $AUTOBUILD_INSTALL_ROOT/share/publican/Common_Content/common
+mkdir -p $AUTOBUILD_INSTALL_ROOT/share/publican/Common_Content
 ln -s /usr/share/publican/Common_Content/common $AUTOBUILD_INSTALL_ROOT/share/publican/Common_Content/common
 make install prefix=$AUTOBUILD_INSTALL_ROOT
 
